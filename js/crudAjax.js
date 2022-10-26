e922e53539c506d2fa66e33c2e655da8a7ac627c
Ruiz, Francisco Mauricio <maumarruiz@hotmail.com>
2022-10-26 12:27:25 -0300

    Crud Ajax 1 - JSON Server

---
+++ js/crudAjax.js
@@ -0,0 +1,119 @@
+const d = document,
+  $table = d.querySelector(".crud-table"),
+  $form = d.querySelector(".crud-form"),
+  $title = d.querySelector(".crud-title"),
+  $template = d.getElementById("crud-template").content,
+  $fragmento = d.createDocumentFragment();
+
+const ajax = (options) => {
+  // validar que options es un objeto válido
+  let { url, method, success, error, data } = options;
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener("readystatechange", (e) => {
+    if (xhr.readyState !== 4) return;
+
+    if (xhr.status >= 200 && xhr.status < 300) {
+      let json = JSON.parse(xhr.responseText);
+      success(json);
+    } else {
+      let message = xhr.statusText || "Ocurrió un error";
+      error(`Error ${xhr.status}: ${message}`);
+    }
+  });
+  xhr.open(method || "GET", url);
+  xhr.setRequestHeader("Content-type", "application/json; charset=utf-9");
+  xhr.send(JSON.stringify(data));
+};
+
+const getAll = () => {
+  ajax({
+    method: "GET",
+    url: "http://localhost:3000/santos",
+    success: (res) => {
+      //console.log(res);
+      res.forEach((el) => {
+        $template.querySelector(".name").textContent = el.nombre;
+        $template.querySelector(".constellation").textContent = el.constelacion;
+        $template.querySelector(".edit").dataset.id = el.id;
+        $template.querySelector(".edit").dataset.name = el.nombre;
+        $template.querySelector(".edit").dataset.constellation =
+          el.constelacion;
+        $template.querySelector(".delete").dataset.id = el.id;
+
+        let $clone = d.importNode($template, true);
+        $fragmento.appendChild($clone);
+      });
+
+      $table.querySelector("tbody").appendChild($fragmento);
+    },
+    error: (err) => {
+      console.error(err);
+      $table.insertAdjacentHTML("afterend", `<p><b>${err}</b></p>`);
+    },
+    data: null,
+  });
+};
+
+d.addEventListener("DOMContentLoaded", getAll);
+
+d.addEventListener("submit", (e) => {
+  if (e.target === $form) {
+    e.preventDefault();
+
+    if (!e.target.id.value) {
+      // POST
+      ajax({
+        method: "POST",
+        url: "http://localhost:3000/santos",
+        success: (res) => location.reload(),
+        error: (err) => {
+          $form.insertAdjacentHTML("afterend", `<p><b>${err}</b></p>`);
+        },
+        data: {
+          nombre: e.target.nombre.value,
+          constelacion: e.target.constelacion.value,
+        },
+      });
+    } else {
+      // PUT
+      ajax({
+        method: "PUT",
+        url: `http://localhost:3000/santos/${e.target.id.value}`,
+        success: (res) => location.reload(),
+        error: (err) => {
+          $form.insertAdjacentHTML("afterend", `<p><b>${err}</b></p>`);
+        },
+        data: {
+          nombre: e.target.nombre.value,
+          constelacion: e.target.constelacion.value,
+        },
+      });
+    }
+  }
+});
+
+d.addEventListener("click", (e) => {
+  if (e.target.matches(".edit")) {
+    $title.textContent = "Editar Santo";
+    $form.nombre.value = e.target.dataset.name;
+    $form.constelacion.value = e.target.dataset.constellation;
+    $form.id.value = e.target.dataset.id;
+  }
+
+  if (e.target.matches(".delete")) {
+    let isDelete = confirm(
+      `¿Estás seguro de eliminar el id ${e.target.dataset.id}?`
+    );
+
+    if (isDelete) {
+      ajax({
+        method: "DELETE",
+        url: `http://localhost:3000/santos/${e.target.dataset.id}?`,
+        success: (res) => location.reload(),
+        error: (err) => {
+          alert(err);
+        },
+      });
+    }
+  }
+});
