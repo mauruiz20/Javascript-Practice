fad457a13fd1eb0932d038fe13225056dfd3ed87
Ruiz, Francisco Mauricio <maumarruiz@hotmail.com>
2022-10-25 23:10:43 -0300

    Asynchrony 1

---
+++ js/ajax.js
@@ -0,0 +1,37 @@
+(() => {
+  const xhr = new XMLHttpRequest(),
+    $xhr = document.getElementById("xhr"),
+    $fragment = document.createDocumentFragment();
+
+  xhr.addEventListener("readystatechange", (e) => {
+    if (xhr.readyState !== 4) return;
+
+    if (xhr.status >= 200 && xhr.status < 300) {
+      //console.log("éxito");
+      //console.log(xhr.responseText);
+      let json = JSON.parse(xhr.responseText);
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+
+      $xhr.appendChild($fragment);
+    } else {
+      //console.log("error");
+      let message = xhr.statusText || "Ocurrió un eror";
+      $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
+    }
+  });
+
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+
+  xhr.send();
+})();
+
+(() => {
+  const $fetch = document.getElementById("fetch"),
+    $fragment = document.createDocumentFragment();
+
+  fetch("https://jsonplaceholder.typicode.com/users");
+})();
