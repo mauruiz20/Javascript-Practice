a7f25e25baf42dc9cc99e7984814b531cde2a0de
Ruiz, Francisco Mauricio <maumarruiz@hotmail.com>
2022-10-29 21:37:16 -0300

    Ajax API Fetch | Stripe
    Pagos Online

---
+++ ajax-ejercicios/assets/stripe-keys.js
@@ -0,0 +1,6 @@
+export default {
+  public:
+    "pk_test_51LyNtqL6718VcJbqXlBE0etv2kYPZT2miY6aGLzzQx7shzsxIRXRm9utKjUvkKukpONaxZ0e9JglMpVdTn1xLdqi00isnP3n4e",
+  secret:
+    "sk_test_51LyNtqL6718VcJbqwzPL8zJ1pql2UYlqoBcC71Rr3gQZApswO99vkuJphlmlgxiIB5ED4gExZ9hKGzYID2t9BUCT00N5M6H1Ug",
+};
