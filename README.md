b7bbb6ca72805e3a522db48867ab9b1ca4315882
Ruiz, Francisco Mauricio <maumarruiz@hotmail.com>
2022-10-30 13:25:14 -0300

    SPA | assets | helpers | components
    Helper a API REST
    Helper a AJAX
    Componentes básicos

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Javascript-Practice
+
+<h2>Prácticas del lenguaje JS Vanilla:</h2>
+<p>Sintaxis, Gramática, Lógica, EcmaScript, Programación Orientada a Objetos, Asincronía, DOM, AJAX, JSON, Consumo de API's, Node y mucho más.</p>
---
+++ spa/app/App.js
@@ -0,0 +1,12 @@
+import api from "./helpers/wp_api.js";
+import { ajax } from "./helpers/ajax.js";
+import { Loader } from "./components/Loader.js";
+import { Header } from "./components/Header.js";
+
+export function App() {
+  const d = document,
+    $root = d.getElementById("root");
+
+  $root.appendChild(Header());
+  $root.appendChild(Loader());
+}
